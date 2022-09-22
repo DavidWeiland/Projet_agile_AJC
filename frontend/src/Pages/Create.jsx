1987e82521c6f651761d1a62281d287a4d6c9496
David Weiland <weiland.david@icloud.com>
2022-09-22 18:56:24 +0200

    add modification style

--- frontend/src/Pages/Create.jsx
+++ frontend/src/Pages/Create.jsx
@@ -41,36 +41,38 @@ export const Create=()=>{
     }
 
     return(
-        <div className="form">
-            <div className="title">Créer un produit</div>
-            <div className="input-container ic1">
-                <input id="title" className="input" type="text" placeholder=" " value={title} onChange={(e)=>setTitle(e.target.value)}/>
-                <div className="cut"></div>
-                <label htmlFor="title" className="placeholder">Titre</label>
+        <div className="createBody">
+            <div className="form">
+                <div className="title">Créer un produit</div>
+                <div className="input-container ic1">
+                    <input id="title" className="input" type="text" placeholder=" " value={title} onChange={(e)=>setTitle(e.target.value)}/>
+                    <div className="cut"></div>
+                    <label htmlFor="title" className="placeholder">Titre</label>
+                </div>
+                <div className="input-container ic2">
+                    <input id="prix" className="input" type="text" placeholder=" " value={price} onChange={(e)=>setPrice(e.target.value)} />
+                    <div className="cut"></div>
+                    <label htmlFor="prix" className="placeholder">Prix en €</label>
+                </div>
+                <div className="input-container ic2">
+                    <input id="description" className="input" type="text" placeholder=" " value={description} onChange={(e)=>setDescription(e.target.value)}/>
+                    <div className="cut cut-short"></div>
+                    <label htmlFor="description" className="placeholder">Description</label>
+                </div>
+                <div className="input-container ic2">
+                    <input id="categorie" className="input" type="text" placeholder=" " value={category} onChange={(e)=>setCategory(e.target.value)}/>
+                    <div className="cut cut-short"></div>
+                    <label htmlFor="categorie" className="placeholder">Catégorie</label>
+                </div>
+                <div className="input-container ic2">
+                    <input id="image" className="input" type="text" placeholder=" " value={image} onChange={(e)=>setImage(e.target.value)}/>
+                    <div className="cut cut-short"></div>
+                    <label htmlFor="image" className="placeholder">ImageUrl</label>
+                    {/* <img src={image} alt='' className="input" style={{objectFit:"cover"}}/>
+                    <input type="file" className="input2" name="imgfile" accept='image/png, image/jpg, image/jpeg' onChange={(e)=>imageReader(e)}/> */}
+                </div>
+                <button type="text" className="submit" onClick={()=>action()}>Créer le produit</button>
             </div>
-            <div className="input-container ic2">
-                <input id="prix" className="input" type="text" placeholder=" " value={price} onChange={(e)=>setPrice(e.target.value)} />
-                <div className="cut"></div>
-                <label htmlFor="prix" className="placeholder">Prix en €</label>
-            </div>
-            <div className="input-container ic2">
-                <input id="description" className="input" type="text" placeholder=" " value={description} onChange={(e)=>setDescription(e.target.value)}/>
-                <div className="cut cut-short"></div>
-                <label htmlFor="description" className="placeholder">Description</label>
-            </div>
-            <div className="input-container ic2">
-                <input id="categorie" className="input" type="text" placeholder=" " value={category} onChange={(e)=>setCategory(e.target.value)}/>
-                <div className="cut cut-short"></div>
-                <label htmlFor="categorie" className="placeholder">Catégorie</label>
-            </div>
-            <div className="input-container ic2">
-                <input id="image" className="input" type="text" placeholder=" " value={image} onChange={(e)=>setImage(e.target.value)}/>
-                <div className="cut cut-short"></div>
-                <label htmlFor="image" className="placeholder">ImageUrl</label>
-                {/* <img src={image} alt='' className="input" style={{objectFit:"cover"}}/>
-                <input type="file" className="input2" name="imgfile" accept='image/png, image/jpg, image/jpeg' onChange={(e)=>imageReader(e)}/> */}
-            </div>
-            <button type="text" className="submit" onClick={()=>action()}>Créer le produit</button>
         </div>
     )
 }
