f56e205db9ec69b04e250726e1319b48129a7989
David Weiland <weiland.david@icloud.com>
2022-09-22 21:09:43 +0200

    fix class to className

--- frontend/src/Component/Footer.jsx
+++ frontend/src/Component/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer=()=>{
     return(
-        <footer class="container fixed-bottom  bg-dark text-light">
-            {/* <div class=" text-center py-3">:
+        <footer className="container fixed-bottom  bg-dark text-light">
+            {/* <div className=" text-center py-3">:
                 </div>  */} 
       </footer>
     )
