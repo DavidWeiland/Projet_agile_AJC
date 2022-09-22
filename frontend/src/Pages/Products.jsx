de13565855067a7da2f74cf657694b3ea50a675d
David Weiland <weiland.david@icloud.com>
2022-09-22 15:28:01 +0200

    elements on react branch

---
+++ frontend/src/Pages/Products.jsx
@@ -0,0 +1,35 @@
+import '../Styles/App.css';
+import {useState, useEffect} from 'react';
+import { Card } from '../Component/Card';
+
+function App() {
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        onclickfunction()
+    }, [])
+
+    const onclickfunction=()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then (json=>setData(json))
+    }
+
+    return (
+        <div className="App">
+        {data?.map(({id, title, category, description, price, image})=>
+            <Card
+            key={`${id}-${title}`}
+            id={id}
+            title = {title}
+            category={category}
+            description={description}
+            price={price}
+            image = {image}
+            />
+        )}
+        </div>
+    );
+}
+
+export default App;
